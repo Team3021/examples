4a32262ec323bc98de2ce3e23f60091fdddb713b
Jason <jassather@gmail.com>
2017-10-01 16:56:00 -0700

    Unstaged  vs Staged Changes? Doing this correctly?

---
+++ preseason-tutorial/src/edu/team3021/restaurant/Restaurant.java
@@ -0,0 +1,11 @@
+package edu.team3021.restaurant;
+
+public class Restaurant {
+	
+	public static void main (String args[]){
+		System.out.println("Hello, please input the order.");
+		
+		//System.exit(0);
+	}
+	
+}

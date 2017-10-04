9d747866af69af61dfc4fc02d2e2ce6e4bf6d5f8
Olivia <Olivia@FireBall>
2017-10-03 20:08:24 -0700

    PJP-11
    Set up scanner for user input (restaurant name)

--- preseason-tutorial/src/edu/team3021/restaurant/Restaurant.java
+++ preseason-tutorial/src/edu/team3021/restaurant/Restaurant.java
@@ -1,11 +1,29 @@
+
 package edu.team3021.restaurant;
+
+//import java.util.Scanner (scanner)
+
+import java.util.Scanner;
 
 public class Restaurant {
 	
 	public static void main (String args[]){
-		System.out.println("Hello, please input the order.");
 		
-		//System.exit(0);
+		//run print line; request resturant name
+		
+		System.out.println("Hello, please input the name of your restaurant.");
+		
+		//run scanner
+		
+		Scanner scanner = new Scanner( System.in );
+		
+		String restaurantName = scanner.nextLine();
+		
+		//close scanner
+		
+		scanner.close();
+	
+		System.out.println("Thank you, welcome to " + restaurantName);
 	}
 	
 }

b719a1ab60af157585cef7d1dc955424fae0330d
Trevor Hawrysh <Trevor.Hawrysh@thermofisher.com>
2017-10-12 20:32:24 -0700

    PJP-15: Storing the restaurant name

--- preseason-tutorial/src/edu/team3021/restaurant/Restaurant.java
+++ preseason-tutorial/src/edu/team3021/restaurant/Restaurant.java
@@ -7,23 +7,47 @@ import java.util.Scanner;
 
 public class Restaurant {
 	
-	public static void main (String args[]){
+	// member attributes
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String nameArg) {
+		// storing the reference to the String object
+		this.name = nameArg;
+	}
+
+	public static void main (String args[]) {
 		
 		//run print line; request resturant name
 		
 		System.out.println("Hello, please input the name of your restaurant.");
 		
-		//run scanner
 		
+		
+		
+		//run scanner
 		Scanner scanner = new Scanner( System.in );
 		
 		String restaurantName = scanner.nextLine();
 		
 		//close scanner
-		
 		scanner.close();
-	
+
+		
+		
+		
+		// create a restaurant object
+		// Type <<variable>> = constructor method of a class
+		Restaurant restaurant = new Restaurant();
+		
+		// giving a string object to the restaurant object
+		restaurant.setName(restaurantName);
+		
 		System.out.println("Thank you, welcome to " + restaurantName);
+		System.out.println("Thank you, welcome to " + restaurant.getName());
 	}
 	
 }

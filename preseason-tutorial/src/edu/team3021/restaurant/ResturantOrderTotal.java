af2c51a9236cba254facd6224bdfe4fe9185e125
Jason <jassather@gmail.com>
2017-11-29 18:54:03 -0800

    Zoe

---
+++ preseason-tutorial/src/edu/team3021/restaurant/ResturantOrderTotal.java
@@ -0,0 +1,14 @@
+package edu.team3021.restaurant;
+
+import java.util.ArrayList;
+
+public class ResturantOrderTotal {
+	double total = 0;
+	final double cheeseburger = 3.50;
+	public void findTotal(ArrayList<String> a){
+		for(String b:a) {
+			System.out.println("This is the Order total class ;)");
+			System.out.println(b);
+		}
+	}
+}
